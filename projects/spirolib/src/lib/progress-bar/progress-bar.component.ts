92d76390066a5f8d3458920bea19e3a0520d5838
spirolad <42509326+spirolad@users.noreply.github.com>
2024-12-15 11:23:13 +0100

    Add progress-bar component & Add eslint

--- projects/spirolib/src/lib/progress-bar/progress-bar.component.ts
+++ projects/spirolib/src/lib/progress-bar/progress-bar.component.ts
@@ -1,12 +1,18 @@
 import {Component, Input} from '@angular/core';
+import {NgIf} from "@angular/common";
+
+type ProgressBarMode = 'determinate' | 'indeterminate';
 
 @Component({
     selector: 'sp-progress-bar',
-    imports: [],
+  imports: [
+    NgIf
+  ],
     templateUrl: './progress-bar.component.html',
     styleUrl: './progress-bar.component.css'
 })
 export class ProgressBarComponent {
-  @Input() progress: number = 0;
-  @Input() displayValue: boolean = false;
+  @Input() progress = 0;
+  @Input() displayValue = false;
+  @Input() mode: ProgressBarMode = 'determinate';
 }
