ce292e7d0da92c34160eea42a3ab8ae2153f53e7
spirolad <42509326+spirolad@users.noreply.github.com>
2024-12-15 11:59:00 +0100

    Fix CI/CD

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,14 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     browsers: ['ChromeHeadless'],
     singleRun: true,
+    reporters: ['progress'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ]
   });
 };
