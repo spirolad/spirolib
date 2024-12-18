87df0e3398d7aaf1ce702180316f45cd281037a7
spirolad <42509326+spirolad@users.noreply.github.com>
2024-12-19 00:00:25 +0100

    Add HTML reporters

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
+        { type: 'html' },
         { type: 'text-summary' },
       ]
     },
